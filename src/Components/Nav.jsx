f1e97c45a069fdfc503a1daf64eea6ee41867ad0
Amit <you@example.com>
2024-05-14 23:32:34 +0530

    first

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
   const content = (
     <>
       <div className="lg:hidden absolute block top-16 w-full left-0 right-0 bg-slate-900 transition">
-        <ul className="text-center text-xl p-20">
+        <ul className="text-center text-xl p-5">
           <Link spy={true} smooth={true} to="Home">
             <li className="my-4 py-4 border-b border-slate-800 hover:bg-slate-800 hover:rounded hover:text-fuchsia-600">
               Home
